6ca11e106d905c14c2634b1b1f6f1d1cfdc6e573
alanhe <qianghe421@gmail.com>
2024-02-08 15:48:48 +0800

    update

---
+++ main.swift
@@ -0,0 +1,27 @@
+// AirPlayBrowser.swift
+import Foundation
+
+class AirPlayBrowser: NSObject, NetServiceBrowserDelegate {
+    private let netServiceBrowser = NetServiceBrowser()
+
+    override init() {
+        super.init()
+        netServiceBrowser.delegate = self
+        netServiceBrowser.searchForServices(ofType: "_airplay._tcp", inDomain: "local.")
+    }
+
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        print("Found AirPlay device: \(service.name)")
+        if !moreComing {
+            netServiceBrowser.stop()
+            exit(0)
+        }
+    }
+
+    func netServiceBrowser(_ browser: NetServiceBrowser, didNotSearch errorDict: [String : NSNumber]) {
+        print("Search not successful: \(errorDict)")
+    }
+}
+
+let airPlayBrowser = AirPlayBrowser()
+RunLoop.main.run()
